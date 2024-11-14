ecefd72a72d67d204eceb861dd4426046053fe97
sundeeep <sandeepkumar25820@gmail.com>
2024-11-14 20:01:20 +0530

    Postgress Pooling added!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-// mod controllers;
-// mod db;
-// mod middlewares;
-mod models;
-// mod routes;
-// mod schemas;
-// mod utils;
-
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use sqlx::postgres::PgPoolOptions;
 use dotenv::dotenv;
 
 #[tokio::main]
-async fn main() {
+async fn main(){
     dotenv().ok();
     let db_url = std::env::var("DB_URL").expect("DB_URL must be set");
-    let _pool: Pool<Postgres> = match PgPoolOptions::new()
+    let _pool= match PgPoolOptions::new()
                 .max_connections(10)
                 .connect(&db_url)
                 .await
