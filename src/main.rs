f67ca5ae945e080dd27b3383ae829057c40902d7
sundeeep <sandeepkumar25820@gmail.com>
2024-11-16 11:27:11 +0530

    /hello route created in axum

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-use sqlx::postgres::PgPoolOptions;
-use dotenv::dotenv;
+use axum::response::Html;
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+
 
 #[tokio::main]
 async fn main(){
-    dotenv().ok();
-    let db_url = std::env::var("DB_URL").expect("DB_URL must be set");
-    let _pool= match PgPoolOptions::new()
-                .max_connections(10)
-                .connect(&db_url)
-                .await
-    {
-        Ok(pool) => {
-            println!("💯Connection to the database is successful!");
-            pool
-        }
-        Err(err) => {
-            println!("/n🔥 Failed to connect to the database: {:?}", err);
-            std::process::exit(1);
-        }
-    };
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("->> LISTENING ON {:?}\n", listener.local_addr());
+    axum::serve(listener, routes_hello.into_make_service())
+    .await
+    .unwrap();
 }
