c871d2d2b89559f6654929213c99c9cf14608cb4
sundeeep <sandeepkumar25820@gmail.com>
2024-11-16 17:15:10 +0530

    Main.rs is ready afer all the tools.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-use axum::response::Html;
 use axum::Router;
 use axum::routing::get;
+
 use tokio::net::TcpListener;
 
+use std::error::Error;
 
+mod models;
+mod routes;
+mod controllers;
+use controllers::hello_controller::{handler_query, handler_path};
 
 #[tokio::main]
-async fn main(){
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
-    );
+async fn main() -> Result<(), Box<dyn Error>>{
 
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    println!("->> LISTENING ON {:?}\n", listener.local_addr());
-    axum::serve(listener, routes_hello.into_make_service())
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_query))
+        .route("/hello/:name", get(handler_path));
+
+    let tcp_listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Could not initialize TcpListener.");
+
+    let local_address = tcp_listener.local_addr().expect("Couldn't convert tcp_listener address to local address.");
+    
+    println!("->> LISTENING ON {:?}\n", local_address);
+    
+    axum::serve(tcp_listener, routes_hello)
     .await
-    .unwrap();
+    .expect("Axum Server is not running!");
+
+    Ok(())
 }
