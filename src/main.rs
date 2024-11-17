9586d77f35f59c00f6d964f55c6ae9f07e0541c8
sundeeep <sandeepkumar25820@gmail.com>
2024-11-17 18:33:53 +0530

    Tracing and ENV config added!

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use axum::Router;
 use axum::routing::get;
 
 use tokio::net::TcpListener;
-
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 use std::error::Error;
+// use tracing::debug;
+
+use dotenv::dotenv;
+use std::env;
 
 mod models;
 mod routes;
@@ -12,22 +17,35 @@ use controllers::hello_controller::{handler_query, handler_path};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>{
+    dotenv().ok();
 
-    let routes_hello = Router::new()
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| "link_shortener=debug".into())
+        )
+        .with(
+            tracing_subscriber::fmt::layer()
+        ).init();
+
+    let app = Router::new()
         .route("/hello", get(handler_query))
         .route("/hello/:name", get(handler_path));
+    
+    let port: String = env::var("PORT").unwrap_or_else(|_| String::from("8002"));
 
-    let tcp_listener = TcpListener::bind("0.0.0.0:3000")
+    let tcp_listener = TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .expect("Could not initialize TcpListener.");
-
-    let local_address = tcp_listener.local_addr().expect("Couldn't convert tcp_listener address to local address.");
     
-    println!("->> LISTENING ON {:?}\n", local_address);
+        println!(
+        "->> Server is Listening on {:?}",
+        tcp_listener.local_addr().expect("Couldn't convert tcp_listener address to local address.")
+    );
     
-    axum::serve(tcp_listener, routes_hello)
-    .await
-    .expect("Axum Server is not running!");
+    axum::serve(tcp_listener, app)
+        .await
+        .expect("Axum Server is not running!");
 
     Ok(())
 }
